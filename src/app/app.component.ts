8163037938f055b6af21ab57def733b1cfae1d4b
Igor Lisboa <igorlisboa34@gmail.com>
2018-03-11 20:53:16 -0300

    desfazendo internacionalização temporariamente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,6 @@ import {Platform} from 'ionic-angular';
 import {StatusBar} from '@ionic-native/status-bar';
 import {SplashScreen} from '@ionic-native/splash-screen';
 import {LoginPage} from "../pages/login/login";
-import {TranslateService} from "ng2-translate";
 
 @Component({
   templateUrl: 'app.html'
@@ -11,10 +10,10 @@ import {TranslateService} from "ng2-translate";
 export class MyApp {
   rootPage:any = LoginPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, translateService: TranslateService) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
-      translateService.setDefaultLang('pt-br');
-      translateService.use('pt-br');
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
-import { HttpClientModule } from '@angular/common/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {ErrorHandler, NgModule} from '@angular/core';
+import {IonicApp, IonicErrorHandler, IonicModule} from 'ionic-angular';
+import {SplashScreen} from '@ionic-native/splash-screen';
+import {StatusBar} from '@ionic-native/status-bar';
+import {HttpClientModule} from '@angular/common/http';
 import * as firebase from 'firebase';
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { LoginPage } from '../pages/login/login';
-import { LoginProvider } from '../providers/login/login';
+import {MyApp} from './app.component';
+import {HomePage} from '../pages/home/home';
+import {LoginPage} from '../pages/login/login';
+import {LoginProvider} from '../providers/login/login';
 import {RegistrarPage} from "../pages/registrar/registrar";
-import { TarefaProvider } from '../providers/tarefa/tarefa';
+import {TarefaProvider} from '../providers/tarefa/tarefa';
 import {TarefaListItemComponent} from "../components/tarefa-list-item/tarefa-list-item";
 import {TarefasAddPage} from "../pages/tarefas-add/tarefas-add";
 import {TarefasListPage} from "../pages/tarefas-list/tarefas-list";
-import { LovProvider } from '../providers/lov/lov';
-import {TranslateLoader, TranslateModule, TranslateStaticLoader} from "ng2-translate";
-import {Http} from "@angular/http";
+import {LovProvider} from '../providers/lov/lov';
 
 const FirebaseConfig = {
   apiKey: "AIzaSyCtH2NDegOKKnbu7P5F_E2fBm0oK9pdffM",
@@ -27,10 +25,6 @@ const FirebaseConfig = {
   storageBucket: "listadordetarefas-a7095.appspot.com",
   messagingSenderId: "973001995830"
 };
-
-export function createTranslateLoader(http:Http){
-  return new TranslateStaticLoader(http, '../assets/i18n', '.json');
-}
 
 @NgModule({
   declarations: [
@@ -45,12 +39,7 @@ export function createTranslateLoader(http:Http){
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
-    HttpClientModule,
-    TranslateModule.forRoot({
-      provide: TranslateLoader,
-      useFActory : (createTranslateLoader),
-      deps: [Http]
-    })
+    HttpClientModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
