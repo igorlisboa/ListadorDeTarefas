bca566ef83268a903013f949632d27f794bab104
Igor Lisboa <Igor Lisboa>
2018-03-10 22:07:18 -0300

    atualizacao do login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -5,7 +5,6 @@ import {LoginProvider} from "../../providers/login/login";
 import {Credencial} from "../../models/credencial";
 import * as _ from "lodash";
 import {TarefasListPage} from "../tarefas-list/tarefas-list";
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @IonicPage()
 @Component({
@@ -15,16 +14,11 @@ import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 export class LoginPage {
 
   credencial : Credencial = _.defaults();
-  loginForm: FormGroup;
 
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
-              public loginProvider : LoginProvider,
-              private formBuilder:FormBuilder) {
-    this.loginForm = this.formBuilder.group({
-      email : [this.credencial.email, Validators.compose([Validators.required, Validators.maxLength(50)])],
-      senha : [this.credencial.senha, Validators.compose([Validators.required, Validators.maxLength(30), Validators.minLength(6)])]
-    })
+              public loginProvider : LoginProvider) {
+
   }
 
   ionViewDidLoad() {
@@ -42,6 +36,9 @@ export class LoginPage {
 
   loginComCredencial(){
     this.loginProvider.loginComCredencial(this.credencial)
+      // .then(response =>{
+      //   this.navCtrl.setRoot()
+      // })
   }
 
   loginComGoogle(){
